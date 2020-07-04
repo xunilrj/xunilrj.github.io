986dc90e3447e195a519b392d2f56132a376fa13
Daniel Leite <1985.daniel@gmail.com>
2020-07-04 10:08:09 +0100

    .

--- frontend/build.sh
+++ frontend/build.sh
@@ -4,6 +4,7 @@ cp node_modules/@fortawesome/fontawesome-free/css/all.min.css ./build/all.min.cs
 cp node_modules/magic.css/dist/magic.css ./build/magic.css -f
 mkdir ./build/webfonts/
 cp node_modules/@fortawesome/fontawesome-free/webfonts/* ./build/webfonts/
+find . -type f -print0 | xargs -0 sed -i 's/common.js/common.js/g'
 pushd build
 rm ../../__snowpack__ -rf
 rm ../../spinningcube -rf
